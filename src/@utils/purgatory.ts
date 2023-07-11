bb2257c60a6065d305fa4a88babea3c449d3e718
Simon Pokorny <simon.pokorny@outlook.com>
2023-07-11 22:24:05 +0200

    deactivated pugratory

--- src/@utils/purgatory.ts
+++ src/@utils/purgatory.ts
@@ -10,8 +10,6 @@ export interface PurgatoryDataAccount {
 export async function getAccountPurgatoryData(
   address: string
 ): Promise<PurgatoryDataAccount> {
-  const data = (await fetchData(
-    `${purgatoryUrl}account?address=${address}`
-  )) as PurgatoryDataAccount[]
+  const data = [] as PurgatoryDataAccount[]
   return { address: data[0]?.address, reason: data[0]?.reason }
 }
