0fd2d478aac287650727c8049c00e104688bccb0
Simon Pokorny <simon.pokorny@outlook.com>
2023-07-12 00:51:28 +0200

    added new address to whitelist and added infura api key

--- address.config.js
+++ address.config.js
@@ -2,7 +2,8 @@ module.exports = {
   whitelists: {
     'nft.owner': [
       '0x2ee3c4F19f90237B7C45cfAD6B5dC4b5840563Ec',
-      '0x2650e382770A04bE0f7E362ed578FB261A60F4b3'
+      '0x2650e382770A04bE0f7E362ed578FB261A60F4b3',
+      '0x279e112d0D182A7234Fe11D0363B3C6f2edd71e4'
     ],
     'datatokens.address': []
   },
