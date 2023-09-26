703191e4cda1f81bab189a66fcc27e459f45117e
shariquetaj <92586768+shariquetaj@users.noreply.github.com>
2023-09-26 11:11:31 +0200

    Update app.config.js (added Sepolia)

--- app.config.js
+++ app.config.js
@@ -14,7 +14,7 @@ module.exports = {
   chainIds: [5],
   infuraProjectId: process.env.NEXT_PUBLIC_INFURA_PROJECT_ID || '5ffc47f65c4042ce847ef66a3fa70d4c',
   // List of all supported chainIds. Used to populate the Chains user preferences list.
-  chainIdsSupported: [5],
+  chainIdsSupported: [1, 137, 5, 80001, 11155111],
 
   defaultDatatokenTemplateIndex: 2,
   // The ETH address the marketplace fee will be sent to.
