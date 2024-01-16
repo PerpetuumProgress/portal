dca0897a4286873ad563e825bd0ea2bd56227c1d
shariquetaj <sharique.taj@pepro.io>
2024-01-16 10:19:35 +0530

    added infuraprojectid

--- app.config.js
+++ app.config.js
@@ -11,8 +11,10 @@ module.exports = {
 
   // List of chainIds which metadata cache queries will return by default.
   // This preselects the Chains user preferences.
-  chainIds: [1, 137, 10, 5],
-
+  chainIds: [5, 1, 137, 10],
+  infuraProjectId:
+    process.env.NEXT_PUBLIC_INFURA_PROJECT_ID ||
+    '5ffc47f65c4042ce847ef66a3fa70d4c',
   // List of all supported chainIds. Used to populate the Chains user preferences list.
   chainIdsSupported: [1, 137, 10, 5, 80001, 11155111],
 
