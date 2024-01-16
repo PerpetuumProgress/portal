1538ad2199923728cf409a24a7bb177111f813d0
shariquetaj <sharique.taj@pepro.io>
2024-01-16 10:11:31 +0530

    resolved conflict in app.config.js

--- app.config.js
+++ app.config.js
@@ -11,18 +11,12 @@ module.exports = {
 
   // List of chainIds which metadata cache queries will return by default.
   // This preselects the Chains user preferences.
-<<<<<<< HEAD
-  chainIds: [5],
-  infuraProjectId: process.env.NEXT_PUBLIC_INFURA_PROJECT_ID || '5ffc47f65c4042ce847ef66a3fa70d4c',
-=======
-  chainIds: [1, 137, 10],
+  chainIds: [1, 137, 10, 5],
 
->>>>>>> upstream/main
   // List of all supported chainIds. Used to populate the Chains user preferences list.
   chainIdsSupported: [1, 137, 10, 5, 80001, 11155111],
 
   customProviderUrl: process.env.NEXT_PUBLIC_PROVIDER_URL,
-
 
   defaultDatatokenTemplateIndex: 2,
   // The ETH address the marketplace fee will be sent to.
