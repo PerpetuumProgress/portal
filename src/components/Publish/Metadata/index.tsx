2c88efce732e22d91276cfea4ade5df154619e6d
w-pepro <willy.tan@pepro.io>
2024-07-23 21:12:13 +0200

    add new sensor section

--- src/components/Publish/Metadata/index.tsx
+++ src/components/Publish/Metadata/index.tsx
@@ -1,7 +1,7 @@
 import { BoxSelectionOption } from '@shared/FormInput/InputElement/BoxSelection'
 import Input from '@shared/FormInput'
 import { Field, useField, useFormikContext } from 'formik'
-import React, { ReactElement, useEffect } from 'react'
+import React, { ReactElement, useEffect, useState } from 'react'
 import content from '../../../../content/publish/form.json'
 import consumerParametersContent from '../../../../content/publish/consumerParameters.json'
 import { FormPublishData } from '../_types'
@@ -10,6 +10,7 @@ import IconScenarioDataset from '@images/scenario.svg'
 import IconTrackDataset from '@images/traffic.svg'
 import IconEnvironmentDataset from '@images/Environment.svg'
 import IconAlgorithm from '@images/algorithm.svg'
+import IconSensorDataset from '@images/sensor.svg'
 import styles from './index.module.css'
 import { algorithmContainerPresets } from '../_constants'
 import Alert from '@shared/atoms/Alert'
@@ -25,6 +26,7 @@ const datasettypeOptionsTitles = getFieldContent(
   'datasettype',
   content.metadata.fields
 ).options
+
 export default function MetadataFields(): ReactElement {
   const { siteContent } = useMarketMetadata()
 
@@ -32,6 +34,7 @@ export default function MetadataFields(): ReactElement {
   const { values, setFieldValue } = useFormikContext<FormPublishData>()
 
   const [field, meta] = useField('metadata.dockerImageCustomChecksum')
+  const [showOptionalFields, setShowOptionalFields] = useState(false)
 
   // BoxSelection component is not a Formik component
   // so we need to handle checked state manually.
@@ -66,12 +69,18 @@ export default function MetadataFields(): ReactElement {
     {
       name: datasettypeOptionsTitles[2].toLowerCase(),
       title: datasettypeOptionsTitles[2],
-      checked: values.metadata.datasettype === 'environment model',
+      checked: values.metadata.datasettype === 'environment',
       icon: <IconEnvironmentDataset />
     },
     {
       name: datasettypeOptionsTitles[3].toLowerCase(),
       title: datasettypeOptionsTitles[3],
+      checked: values.metadata.datasettype === 'sensor',
+      icon: <IconSensorDataset />
+    },
+    {
+      name: datasettypeOptionsTitles[4].toLowerCase(),
+      title: datasettypeOptionsTitles[4],
       checked: values.metadata.datasettype === 'custom shape',
       icon: <IconDataset />
     }
@@ -351,7 +360,7 @@ export default function MetadataFields(): ReactElement {
           </>
         )}
       {values.metadata.type === 'dataset' &&
-        values.metadata.datasettype === 'environment model' && (
+        values.metadata.datasettype === 'environment' && (
           <>
             <Field
               {...getFieldContent(
@@ -476,6 +485,73 @@ export default function MetadataFields(): ReactElement {
           </>
         )}
       {values.metadata.type === 'dataset' &&
+        values.metadata.datasettype === 'sensor' && (
+          <>
+            <Field
+              {...getFieldContent(
+                'authorOfSensorData',
+                content.metadata.fields
+              )}
+              component={Input}
+              name="metadata.authorOfSensorData"
+              label="Author of Sensor Data"
+              example="prefix:AuthorIRI"
+            />
+            <Field
+              {...getFieldContent('localization', content.metadata.fields)}
+              component={Input}
+              name="metadata.localization"
+              label="SRMD Localization"
+              example="prefix:LocalizationIRI"
+            />
+            <Field
+              {...getFieldContent('environment', content.metadata.fields)}
+              component={Input}
+              name="metadata.environment"
+              label="SRMD Environment"
+              example="prefix:EnvironmentIRI"
+            />
+            <Field
+              {...getFieldContent('producedBySensor', content.metadata.fields)}
+              component={Input}
+              name="metadata.producedBySensor"
+              label="Sensor"
+              example="prefix:SensorIRI"
+            />
+            <Field
+              {...getFieldContent('authorContact', content.metadata.fields)}
+              component={Input}
+              name="metadata.authorContact"
+              label="Author Contact"
+              example="<+49 1234 456789> or <example@examplecompany.com>"
+            />
+            <Field
+              {...getFieldContent(
+                'sensorDataDocumentLink',
+                content.metadata.fields
+              )}
+              component={Input}
+              name="metadata.sensorDataDocumentLink"
+              label="Sensor Data Document Link"
+              example="https://www.hierLiegtEinDocument.org/document..."
+            />
+            <Field
+              {...getFieldContent('modelLink', content.metadata.fields)}
+              component={Input}
+              name="metadata.modelLink"
+              label="Sensor Data Document"
+              example="prefix:SensorDataFileIRI"
+            />
+            <Field
+              {...getFieldContent('sensorName', content.metadata.fields)}
+              component={Input}
+              name="metadata.sensorName"
+              label="Sensor Name"
+              example="ARS548"
+            />
+          </>
+        )}
+      {values.metadata.type === 'dataset' &&
         values.metadata.datasettype === 'scenario' && (
           <>
             <Field
@@ -590,6 +666,188 @@ export default function MetadataFields(): ReactElement {
               name="metadata.consumerParameters"
             />
           )}
+        </>
+      )}
+
+      <Field
+        {...getFieldContent('showOptionalFields', content.metadata.fields)}
+        component={Input}
+        name="metadata.showOptionalFields"
+        type="checkbox"
+        onChange={() => setShowOptionalFields(!showOptionalFields)}
+      />
+
+      {showOptionalFields && (
+        <>
+          <Field
+            {...getFieldContent('timeReferenceFrame', content.metadata.fields)}
+            component={Input}
+            name="metadata.timeReferenceFrame"
+          />
+          <Field
+            {...getFieldContent('timeDuration', content.metadata.fields)}
+            component={Input}
+            name="metadata.timeDuration"
+          />
+          <Field
+            {...getFieldContent('timeStartTime', content.metadata.fields)}
+            component={Input}
+            name="metadata.timeStartTime"
+          />
+          <Field
+            {...getFieldContent('usedToolchain', content.metadata.fields)}
+            component={Input}
+            name="metadata.usedToolchain"
+          />
+          <Field
+            {...getFieldContent('sensorTestsetup', content.metadata.fields)}
+            component={Input}
+            name="metadata.sensorTestsetup"
+          />
+          <Field
+            {...getFieldContent('sensorDataContent', content.metadata.fields)}
+            component={Input}
+            name="metadata.sensorDataContent"
+          />
+          <Field
+            {...getFieldContent(
+              'gpsTraceDocumentLink',
+              content.metadata.fields
+            )}
+            component={Input}
+            name="metadata.gpsTraceDocumentLink"
+          />
+          <Field
+            {...getFieldContent('modelLink', content.metadata.fields)}
+            component={Input}
+            name="metadata.modelLink"
+          />
+          <Field
+            {...getFieldContent(
+              'sensorTraceDocumentLink',
+              content.metadata.fields
+            )}
+            component={Input}
+            name="metadata.sensorTraceDocumentLink"
+          />
+          <Field
+            {...getFieldContent(
+              'sensorDataRawDocumentLink',
+              content.metadata.fields
+            )}
+            component={Input}
+            name="metadata.sensorDataRawDocumentLink"
+          />
+          <Field
+            {...getFieldContent(
+              'groundTruthDocumentLink',
+              content.metadata.fields
+            )}
+            component={Input}
+            name="metadata.groundTruthDocumentLink"
+          />
+          <Field
+            {...getFieldContent('authorGivenName', content.metadata.fields)}
+            component={Input}
+            name="metadata.authorGivenName"
+            label="Author Given Name"
+          />
+          <Field
+            {...getFieldContent('authorFamilyName', content.metadata.fields)}
+            component={Input}
+            name="metadata.authorFamilyName"
+            label="Author Family Name"
+          />
+          <Field
+            {...getFieldContent('authorCompany', content.metadata.fields)}
+            component={Input}
+            name="metadata.authorCompany"
+            label="Author Company"
+          />
+          <Field
+            {...getFieldContent('sensorDataFileSize', content.metadata.fields)}
+            component={Input}
+            name="metadata.sensorDataFileSize"
+            label="Sensor Data File Size (in KB)"
+          />
+          <Field
+            {...getFieldContent(
+              'sensorDataFileChecksum',
+              content.metadata.fields
+            )}
+            component={Input}
+            name="metadata.sensorDataFileChecksum"
+            label="Sensor Data File Checksum"
+          />
+          <Field
+            {...getFieldContent(
+              'sensorDataFileMimetype',
+              content.metadata.fields
+            )}
+            component={Input}
+            name="metadata.sensorDataFileMimetype"
+            label="Sensor Data File Mimetype"
+          />
+          <Field
+            {...getFieldContent('sensorType', content.metadata.fields)}
+            component={Input}
+            name="metadata.sensorType"
+            label="Sensor Type"
+          />
+          <Field
+            {...getFieldContent(
+              'sensorHardwareVersion',
+              content.metadata.fields
+            )}
+            component={Input}
+            name="metadata.sensorHardwareVersion"
+            label="Sensor Hardware Version"
+          />
+          <Field
+            {...getFieldContent(
+              'sensorFirmwareVersion',
+              content.metadata.fields
+            )}
+            component={Input}
+            name="metadata.sensorFirmwareVersion"
+            label="Sensor Firmware Version"
+          />
+          <Field
+            {...getFieldContent(
+              'sensorManufacturerPartnumber',
+              content.metadata.fields
+            )}
+            component={Input}
+            name="metadata.sensorManufacturerPartnumber"
+            label="Sensor Manufacturer Partnumber"
+          />
+          <Field
+            {...getFieldContent('sensorTechnology', content.metadata.fields)}
+            component={Input}
+            name="metadata.sensorTechnology"
+            label="Sensor Technology"
+          />
+          <Field
+            {...getFieldContent(
+              'sensorTechnologyVariant',
+              content.metadata.fields
+            )}
+            component={Input}
+            name="metadata.sensorTechnologyVariant"
+            label="Sensor Technology Variant"
+          />
+          <Field
+            {...getFieldContent('sensorStatus', content.metadata.fields)}
+            component={Input}
+            name="metadata.sensorStatus"
+            label="Sensor Status"
+          />
+          <Field
+            {...getFieldContent('sensorPurpose', content.metadata.fields)}
+            component={Input}
+            name="metadata.sensorPurpose"
+            label="Sensor Purpose"
+          />
         </>
       )}
 
